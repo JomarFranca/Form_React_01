75ccabb1e645467c9f7dd321150bfdcebd539d98
Jomar A. França <jomar.almeida@gmail.com>
2022-07-08 00:40:45 -0300

    Criado input CPF

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
 	const [client, setClient] = useState();
 	const [client2, setClient2] = useState();
 	const [mail, setMail] = useState();
+	const [clientCpf, setClientCpf] = useState();
 
 	const [enviado, setEnviado] = useState(false);
 
@@ -31,14 +32,21 @@ function App() {
 				value={mail}
 				onChange={(e) => setMail(e.target.value)}
 				placeholder="Digite um email:" />
+			<label>CPF:</label>
+			<input type="number"
+				id="cpf"
+				value={clientCpf}
+				onChange={(e) => setClientCpf(e.target.value)} 
+				data-mask="999.999.999-99"
+				required placeholder="000.000.000-00" />
 
 			<button onClick={() => setEnviado(true)} >Enviar</button>
-			<p>Nome:{enviado && client + ' ' + client2}</p>
-			<p>E-mail:{enviado && mail}</p>
-			<p>CPF:</p>
-			<p>Data de Nascimento:</p>
-			<p>Login:</p>
-			<p>Senha:</p>
+			<p>Nome: {enviado && client + ' ' + client2}</p>
+			<p>E-mail: {enviado && mail}</p>
+			<p>CPF: {enviado && clientCpf}</p>
+			<p>Data de Nascimento: </p>
+			<p>Login: </p>
+			<p>Senha: </p>
 		</>
 	);
 }
